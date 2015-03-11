473eb383044b5abd58e53039cc87fe13c2db6de9
Lars Andrén <andren.lars@gmail.com>
2015-03-11 14:22:20 +0100

    Update differentBits.java

--- differentBits.java
+++ differentBits.java
@@ -1,4 +1,12 @@
+/**
+ * Calculat the number of bits in order to convert integer a to integer b.
+ * Could also use StringUtils, if Apache Commons is available.
+ */
 public int differentBits(int a, int b) {
 
+  int xOR = a ^ b;
   
+  String xORInBinary = Integer.toString(xOR, 2);
+
+	return xORInBinary.length() - xORInBinary.replace("1", "").length();
 }
