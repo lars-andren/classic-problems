0e06c80d1fd289f5fc4ded0045e9b37b9ffdb715
Lars Andrén <andren.lars@gmail.com>
2015-12-15 16:34:07 +0100

    Update convertInteger.java

--- convertInteger.java
+++ convertInteger.java
@@ -1,12 +1,7 @@
 /**
  * Calculat the number of bits in order to convert integer a to integer b.
- * Could also use StringUtils, if Apache Commons is available.
  */
 public int convertInteger(int a, int b) {
 
-  int xOR = a ^ b;
-  
-  String xORInBinary = Integer.toString(xOR, 2);
-  
-  return xORInBinary.length() - xORInBinary.replace("1", "").length();
+  return Integer.bitCount(a ^ b);
 }
