76e1e73e1cfda25cb4f645ad81b49ddc94f5efb5
Lars Andrén <andren.lars@gmail.com>
2019-03-28 14:55:10 +0100

    Update oddElements.java

--- oddElements.java
+++ oddElements.java
@@ -1,4 +1,4 @@
-    /* Returns a set of the Integers that occurred an odd number of times in the input List*/
+    /* Returns a set of the Integers that occurred an odd number of times in the input List */
     public Set<Integer> oddElements(List<Integer> input) throws Exception {
 
         HashSet<Integer> set = new HashSet<Integer>();
